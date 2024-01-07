959185ee58b81ca29c1b7d9b1a6235d9dca0eda6
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2024-01-07 22:51:36 +0900

    240107_공통 레이아웃 설정

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 
 function AboutPage(props) {
   return (
-    <div className={"text-3xl"}>
-      <div>
-        <Link to={"/"}>Main</Link>
-      </div>
-      <div>About Page</div>
-    </div>
+    <BasicLayout>
+      <div className={"text-3xl"}>About Page</div>
+    </BasicLayout>
   );
 }
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 
 function MainPage(props) {
   return (
-    <div className={"text-3xl"}>
-      <div>
-        <Link to={"/about"}>About</Link>
-      </div>
-      <div>Main Page</div>
-    </div>
+    <BasicLayout>
+      <div className={"text-3xl"}>Main Page</div>
+    </BasicLayout>
   );
 }
 
