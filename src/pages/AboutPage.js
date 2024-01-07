dac072db2bab60c60f531bfd835b67eb621e4adc
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2024-01-07 21:07:16 +0900

    240107_Link 태그

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function AboutPage(props) {
-  return <div className={"text-3xl"}>About Page</div>;
+  return (
+    <div className={"text-3xl"}>
+      <div>
+        <Link to={"/"}>Main</Link>
+      </div>
+      <div>About Page</div>
+    </div>
+  );
 }
 
 export default AboutPage;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MainPage(props) {
   return (
     <div className={"text-3xl"}>
+      <div>
+        <Link to={"/about"}>About</Link>
+      </div>
       <div>Main Page</div>
     </div>
   );
